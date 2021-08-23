e4bbd8fe8eb7ed13e3683dd48033120bcb021328
Martha Arbayani Zaidan <martha_arbayani@yahoo.com>
2021-08-23 13:21:41 +0800

    Second commit

--- SenWork_Analysis_v1.py
+++ SenWork_Analysis_v1.py
@@ -11,19 +11,68 @@ Created on Mon Aug 23 11:06:36 2021
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
-#%% We read data from SORPES:
+#%% We read data from Nanjing Sensor Network Data:
     
 xls = pd.ExcelFile('DATA/2021_Mar-Jul_AQData_Gulou_district.xlsx')
 SheetNames = xls.sheet_names  # see all sheet names
 #xls.parse(SheetNames)  # read a specific sheet to DataFrame
 DATA1       = pd.read_excel(xls, SheetNames[0])
 DATA1_label = pd.read_excel(xls, SheetNames[1])
-    
+
+
+#%% To check the available variables on each sheet
+print('DATA1 include:')    
+for col in DATA1:
+    print(col)    
+
+print('DATA1+label include:')    
+for col in DATA1_label:
+    print(col)        
+
+
+#%% Check the data, if any missing data or the types of data
+
+DATA1.info()
+DATA1_label.info()
+
+print(DATA1.dtypes)
+
+
+# From the above informaiton, many data contain 'object", we need
+# to change them all to float64, except for cols below: 
+cols = DATA1.columns.drop(['qc_datetime','SITE_NAME', '点位名称', '街道名称', 
+                              '匹配的点位类型']) # This is better
+
+# Next, we use the "cols" names, to make them 'float'
+# Replace blank value with nan and now everything becomes float number
+# https://www.geeksforgeeks.org/python-pandas-to_numeric-method/
+DATA1[cols] = DATA1[cols].apply(pd.to_numeric, errors='coerce')
+print(DATA1.dtypes) 
+
+print(pd.unique(DATA1['CODE']))
+print(pd.unique(DATA1['SITE_NAME']))
+
+CODE = pd.unique(DATA1['CODE'])
+SITE_NAME = pd.unique(DATA1['SITE_NAME'])
+
+# https://stackoverflow.com/questions/21800169/python-pandas-get-index-of-rows-which-column-matches-certain-value
+L = DATA1['CODE'].index[DATA1['CODE'] == 84577].tolist()
+
+#DATA2 = DATA1.groupby("CODE")
 
 
 
+
+pd.isnan(DATA1['CODE'])
+
+idx_CODE_nan = DATA1['CODE'] == np.nan
+
+t = idx_CODE_nan
+[i for i, x in enumerate(t) if x]
+[4, 5, 7]
 
 # Other reading methods: 
 #df_SORPES = pd.read_csv('SORPES data/gas_aerosol_SORPES_2019.xlsx')
